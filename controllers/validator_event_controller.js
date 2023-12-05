d6e9a1f76c19c89e95ac0470fe4fe660ad45d921
vineetbellway <vineetbellway@gmail.com>
2023-12-05 11:38:34 +0530

    modification in APIS

--- controllers/validator_event_controller.js
+++ controllers/validator_event_controller.js
@@ -465,8 +465,8 @@ exports.get_event_validators_list = async (req, res) => {
 
              var validator_status =  val.validator_event_data[0].validator_status;
 
-           //  validator_status =  (validator_status!="pending") ? validator_status+"ed" : '';
-              if(validator_status == "pending"){
+           validator_status =  (validator_status!="pending") ? validator_status+"ed" : '';
+             
                 all_validator_list.push({
                   _id: val._id,
                   user_id: val.user_id,
@@ -481,11 +481,7 @@ exports.get_event_validators_list = async (req, res) => {
                   role : val.validator_event_data[0].role,
                   validator_status:validator_status
                 });
-              }
-
-             
-
-           }
+            }
           });
           if(all_validator_list.length >0){
             res.status(200).send({
@@ -531,154 +527,159 @@ exports.get_not_expired_event_validators_list = async (req, res) => {
   const event_id = req.query.event_id;
 
   try {
-    const seller = await SellerModel.findOne({ user_id: seller_id });
+    var seller = await User.findById(seller_id);
     if (!seller) {
-      return res.status(404).send({
+      return res.status(200).send({
         status: false,
         message: "Seller not found",
         data: null,
       });
     }    
+    var sellerData = await SellerModel.findOne({ user_id: seller_id });
+    
+
+
 
     const event = await EventModel.findById(event_id);
     if (!event) {
-      return res.status(404).send({
+      return res.status(200).send({
         status: false,
         message: "Event not found",
         data: null,
       });
     } 
 
-  
-    var current_date_time = moment().format("YYYY-MM-DDTHH:mm:ss.SSSZ");
-    var event_end_time = moment(event.end_time).format("YYYY-MM-DDTHH:mm:ss.SSSZ");
     var event_status = event.status;
 
     if(event_status == "expiired"){
-      return  res.status(404).send({
+      return  res.status(200).send({
         status: true,
         message: "No validators found for the specified event id",
         data: [],
       });
     }
 
- 
 
+    var current_date_time = moment().format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+    var event_end_time = moment(event.end_time).format("YYYY-MM-DDTHH:mm:ss.SSSZ");
+    var seller_district = sellerData.district;
 
-    var seller_district = seller.district;
-
-    const event_validators = await Validator.aggregate([
+    const event_validators = await User.aggregate([
       {
         $sort: { createdAt: -1 }, // Sort by createdAt in descending order
       },
       {
         $lookup: {
           from: "eventvalidators",
-          localField: "validator_id",
-          foreignField: "user_id",
+          localField: "_id",
+          foreignField: "validator_id",
           as: "validator_event_data",
         },
       },
       {
-        $project: {
-          _id: 1,
-          user_id: 1,
-          full_name: 1,
-          district: 1,
-          state: 1,
-          country: 1,
-          status: 1,
-          createdAt: 1,
-          updatedAt: 1,
-          __v: 1,
-          role: 1,
-          validator_event_data: {
-            $filter: {
-              input: "$validator_event_data",
-              as: "eventData",
-              cond: {
-                $eq: ["$$eventData.validator_id", "$_id"],
-              },
-            },
+        $lookup: {
+          from: "validators", // Assuming the name of your "validator" model
+          localField: "_id", // The field in the "User" model
+          foreignField: "user_id", // The corresponding field in the "validator" model
+          as: "validator_data",
+        },
+      },
+      {
+        $addFields: {
+          type: {
+            $ifNull: [{ $arrayElemAt: ["$validator_event_data.type", 0] }, 'validator'],
           },
+          validator_data: { $arrayElemAt: ["$validator_data", 0] },
+        },
+      },
+      {
+        $match: {
+          "validator_event_data": { $ne: [] } // Filter out documents where validator_event_data is an empty array
         },
       },
     ]);
+    
+  
+
 
     if (event_validators && event_validators.length > 0) {
+   
       const filtered_event_validator_data = event_validators
         .map((validator) => ({
-          _id: validator._id,
-          user_id: validator.user_id,
-          full_name: validator.full_name,
-          district: validator.district,
-          state: validator.state,
-          country: validator.country,
-          status: validator.status,
-          createdAt: validator.createdAt,
-          updatedAt: validator.updatedAt,
-          __v: validator.__v,
-          validator_event_data: validator.validator_event_data.map((eventData) => ({
-            _id: eventData._id,
-            validator_id: eventData.validator_id,
-            seller_id: eventData.seller_id,
-            event_id: eventData.event_id,
-            role: eventData.role,
-            createdAt: eventData.createdAt,
-            updatedAt: eventData.updatedAt,
-            __v: eventData.__v,
-          })),
-        }))
-        .filter((validator) => validator.validator_event_data.some((event) => event.event_id == event_id));
-          var all_validator_list = [];
-        if (filtered_event_validator_data.length > 0) {    
-          var event_end_time = moment(event.end_time).format("YYYY-MM-DDTHH:mm:ss.SSSZ");
 
-        
+
+         _id: validator.validator_data._id,
+          user_id: validator.validator_data.user_id,
+          full_name: validator.validator_data.full_name,
+          district: validator.validator_data.district,
+          state: validator.validator_data.state,
+          country: validator.validator_data.country,
+          status: validator.validator_data.status,
+          createdAt: validator.validator_data.createdAt,
+          updatedAt: validator.validator_data.updatedAt,
+          __v: validator.validator_data.__v,
+          validator_event_data: validator.validator_event_data.map((validatorEventData) => ({
+            _id: validatorEventData._id,
+            validator_id: validatorEventData.validator_id,
+            seller_id: validatorEventData.seller_id,
+            event_id: validatorEventData.event_id,
+            role: validatorEventData.role,
+            createdAt: validatorEventData.createdAt,
+            updatedAt: validatorEventData.updatedAt,
+            __v: validatorEventData.__v,
+            validator_status : validatorEventData.status
+          })),
+        }));
+          var all_validator_list = [];
+    
+        if (filtered_event_validator_data.length > 0) {    
           filtered_event_validator_data.forEach((val, k) => {
+
             if(seller_district == val.district){
 
-              var validator_event_create_date_time  = moment(val.validator_event_data[0].createdAt).format("YYYY-MM-DDTHH:mm:ss.SSSZ");
-              var is_validator_present_in_event = false;
-    
-              console.log('validator_event_create_date_time',validator_event_create_date_time)
-    
-              console.log('event_end_time',event_end_time)
-    
-              if(event_end_time > validator_event_create_date_time){
-                is_validator_present_in_event = true;
-              }
-              all_validator_list.push({
-                _id: val._id,
-                user_id: val.user_id,
-                full_name: val.full_name,
-                district: val.district,
-                state: val.state,
-                country: val.country,
-                status: val.status,
-                createdAt: val.createdAt,
-                updatedAt: val.updatedAt,
-                __v: val.__v,
-                role : val.validator_event_data[0].role,
-                is_validator_present_in_event:is_validator_present_in_event
-              });
+             var validator_status =  val.validator_event_data[0].validator_status;
 
+           validator_status =  (validator_status!="pending") ? validator_status+"ed" : '';
+             
+                all_validator_list.push({
+                  _id: val._id,
+                  user_id: val.user_id,
+                  full_name: val.full_name,
+                  district: val.district,
+                  state: val.state,
+                  country: val.country,
+                  status: val.status,
+                  createdAt: val.createdAt,
+                  updatedAt: val.updatedAt,
+                  __v: val.__v,
+                  role : val.validator_event_data[0].role,
+                  validator_status:validator_status
+                });
             }
           });
+          if(all_validator_list.length >0){
+            res.status(200).send({
+              status: true,
+              message: "Data found",
+              data: all_validator_list,
+            });
+          } else {
+            res.status(200).send({
+              status: true,
+              message: "No validators found",
+              data: [],
+            });
+          }
+          
+        } else {
           res.status(200).send({
             status: true,
-            message: "Data found",
-            data: all_validator_list,
-          });
-        } else {
-          res.status(404).send({
-            status: true,
-            message: "No validators found for the specified event id",
+            message: "No validators found",
             data: filtered_event_validator_data,
           });
         }
     } else {
-      res.status(404).send({
+      res.status(200).send({
         status: true,
         message: "No validators found",
         data: [],
@@ -709,7 +710,7 @@ exports.manage_event_validator_status = async (req, res) => {
     );
 
     if (!result) {
-      return res.status(404).json({ status: false, message: "Event validator not found" });
+      return res.status(200).json({ status: false, message: "Event validator not found" });
     }
 
   
@@ -768,58 +769,71 @@ d;
 
 exports.get_validator_events_list = async (req, res) => {
   const validator_id = req.query.validator_id;
+  const status = req.query.status;
 
   try {
     const validator = await User.findById(validator_id);
 
     if (!validator) {
-      return res.status(404).send({
+      return res.status(200).send({
         status: false,
         message: "Validator not found",
         data: null,
       });
     }
 
-    const validator_events = await EventValidator.find({ validator_id, status: { $ne: 'pending' } });
+    const validator_events = await EventValidator.find({ validator_id, status: { $eq: status } });
 
 
     if (validator_events && validator_events.length > 0) {
       const all_validator_events_list = await Promise.all(validator_events.map(async (validatorEventData) => {
-        const eventDetails = await EventModel.findById(validatorEventData.event_id);
+          const eventDetails = await EventModel.findById(validatorEventData.event_id);
+          // Get the host (domain and port)
+          const protocol = req.protocol;
+          const host = req.get('host');
+
+          // Combine protocol, host, and any other parts of the base URL you need
+          const baseURL = `${protocol}://${host}`;
+          var validator_status = validatorEventData.status;
+          console.log("validator_status",validator_status)
+          
+            return {
+          
+          
+              _id: eventDetails._id,
+              seller_id: eventDetails.seller_id,
+              primary_number: eventDetails.primary_number,
+              secondary_number: eventDetails.secondary_number,
+              type: eventDetails.type,
+              image: baseURL + '/uploads/events/' + eventDetails.image,
+              name: eventDetails.name,
+              venue: eventDetails.venue,
+              country: eventDetails.country,
+              state: eventDetails.state,
+              city: eventDetails.city,
+              start_time: eventDetails.start_time,
+              end_time: eventDetails.end_time,
+              coupon_name: eventDetails.coupon_name,
+              tax_name: eventDetails.tax_name,
+              tax_percent: eventDetails.tax_percent,
+              amount: eventDetails.amount,
+              instructions: eventDetails.instructions,
+              transportation_charge: eventDetails.transportation_charge,
+              hire_charge: eventDetails.hire_charge,
+              labour_charge: eventDetails.labour_charge,
+              commision_charge: eventDetails.commision_charge,
+              others: eventDetails.others,
+              status: eventDetails.status,
+              createdAt: eventDetails.createdAt,
+              updatedAt: eventDetails.updatedAt,
+              __v: eventDetails.__v,
+            
+          };
+          
+
+        
       
-        return {
-          
-          
-            _id: eventDetails._id,
-            seller_id: eventDetails.seller_id,
-            primary_number: eventDetails.primary_number,
-            secondary_number: eventDetails.secondary_number,
-            type: eventDetails.type,
-            image: eventDetails.image,
-            name: eventDetails.name,
-            venue: eventDetails.venue,
-            country: eventDetails.country,
-            state: eventDetails.state,
-            city: eventDetails.city,
-            start_time: eventDetails.start_time,
-            end_time: eventDetails.end_time,
-            coupon_name: eventDetails.coupon_name,
-            tax_name: eventDetails.tax_name,
-            tax_percent: eventDetails.tax_percent,
-            amount: eventDetails.amount,
-            instructions: eventDetails.instructions,
-            transportation_charge: eventDetails.transportation_charge,
-            hire_charge: eventDetails.hire_charge,
-            labour_charge: eventDetails.labour_charge,
-            commision_charge: eventDetails.commision_charge,
-            others: eventDetails.others,
-            status: eventDetails.status,
-            createdAt: eventDetails.createdAt,
-            updatedAt: eventDetails.updatedAt,
-            __v: eventDetails.__v,
-            validator_status: validatorEventData.status+"ed" ,
-          
-        };
+       
       }));
 
       if (all_validator_events_list.length > 0) {
@@ -829,14 +843,14 @@ exports.get_validator_events_list = async (req, res) => {
           data: all_validator_events_list,
         });
       } else {
-        res.status(404).send({
+        res.status(200).send({
           status: true,
           message: "No events found for the validator",
           data: [],
         });
       }
     } else {
-      res.status(404).send({
+      res.status(200).send({
         status: true,
         message: "No events found for the validator",
         data: [],

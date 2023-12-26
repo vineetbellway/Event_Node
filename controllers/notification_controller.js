7e997472def8a7362e2ffde2562daf8fc3471597
vineetbellway <vineetbellway@gmail.com>
2023-12-26 17:18:35 +0530

    pagination added in get notifications API

--- controllers/notification_controller.js
+++ controllers/notification_controller.js
@@ -1,58 +1,69 @@
 const Notification = require("../models/notification.model");
 const mongoose = require("mongoose");
 
+
 const get_notifications = async (req, res) => {
-    var user_id = req.query.user_id;
-    var type = req.query.type;
+  var user_id = req.query.user_id;
+  var type = req.query.type;
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  const myCustomLabels = {
+    totalDocs: "totalDocs",
+    docs: "data",
+    limit: "limit",
+    page: "page",
+    nextPage: "nextPage",
+    prevPage: "prevPage",
+    totalPages: "totalPages",
+    pagingCounter: "slNo",
+    meta: "paginator",
+  };
 
-    if (!user_id) {
-      res.status(400).send({ status: false, message: "user_id missing" });
-    } else {
-      try {
-        await Notification.aggregate([
-          {
-            $match: {
-              to_user_id: new mongoose.Types.ObjectId(user_id),
-              type : type
-            },
-          },
-          {
-            $lookup: {
-              from: "users",
-              localField: "to_user_id",
-              foreignField: "_id",
-              as: "user",
-            },
-          },
-          {
-            $unwind: "$user",
-          },
-        ])
-          .then((result) => {
+  const options = {
+    page: page,
+    limit: limit,
+    customLabels: myCustomLabels,
+  };
 
-            if (result) {
-              res.status(200).send({
-                status: true,
-                message: "success",
-                data: result,
-              });
-            }
-          })
-          .catch((error) => {
-            res.send({
-              status: false,
-              message: error.toString() ?? "Error",
-            });
+  try {
+    var myAggregate = Notification.aggregate([
+      {
+        $match: {
+          "to_user_id": new mongoose.Types.ObjectId(user_id),
+          "type": type,
+        },
+      },
+      {
+        $sort: { createdAt: -1 }, // Sort by createdAt in descending order
+      },
+    ]);
+
+    await Notification.aggregatePaginate(myAggregate, options)
+      .then((result) => {
+        if (result) {
+          res.status(200).send({
+            status: true,
+            message: "success",
+            data: result,
           });
-      } catch (error) {
-        console.log("error",error)
-        res.status(500).send({
+        }
+      })
+      .catch((error) => {
+        res.send({
           status: false,
-          message: error.toString() ?? "Internal Server Error",
+          message: error.toString() ?? "Error",
         });
-      }
-    }
+      });
+  } catch (error) {
+    res.status(500).send({
+      status: false,
+      message: error.toString() ?? "Internal Server Error",
+    });
+  }
 };
+
+
+
 
 const get_unread_notifications_count = async (req, res) => {
     var user_id = req.query.user_id;
@@ -80,6 +91,7 @@ const get_unread_notifications_count = async (req, res) => {
           {
             $unwind: "$user",
           },
+
         ])
           .then((result) => {
             console.log("result",result)

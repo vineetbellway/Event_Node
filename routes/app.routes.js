a76a5cbf053b064777cabdd1edb99c535476019c
vineetbellway <vineetbellway@gmail.com>
2024-01-24 14:40:36 +0530

    added get menu by event id code from git history and take latest code from main branch

--- routes/app.routes.js
+++ routes/app.routes.js
@@ -102,7 +102,13 @@ router.delete("/event",auth,  eventController.delete_events);
 router.post("/menu",auth,menuController.create_menu);
 router.put("/menu/:id", auth, menuController.update_menu);
 router.get("/menu", auth,menuController.get_menus);
-router.get("/menu_by_event_id/:id/:guest_id",auth,menuController.get_menu_by_event_id);
+router.get("/menu_by_event_id/:id/:guest_id", auth,menuController.get_menu_by_event_id);
+
+
+router.get("/menu_by_event_id_before_entry_food_event/:id/:guest_id",menuController.get_menu_by_event_id_before_entry_food_event);
+router.get("/menu_by_event_id_3_weeks_ago/:id/:guest_id",menuController.get_menu_by_event_id_3_weeks_ago);
+
+
 router.get("/menu/:id", auth,menuController.get_menu);
 router.delete("/menu/:id", auth, menuController.delete_menu);
 router.delete("/menu", auth, menuController.delete_menus);
